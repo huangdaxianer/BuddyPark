545a01e2d4db9070a1a1a899374c468ce7fba614
huangdaxianer <huang0623@gmail.com>
2023-09-06 18:02:25 +0800

    现在可以正确地按顺序展示角色

--- BuddyPark/Constants.swift
+++ BuddyPark/Constants.swift
@@ -1,12 +1,6 @@
-//
-//  Constants.swift
-//  BuddyPark
-//
-//  Created by 黄鹏昊 on 2023/8/28.
-//
-
 import Foundation
 
 let appGroupName = "group.com.penghao.BuddyPark"
 let messageService = "https://service-jqp52izs-1251732024.jp.apigw.tencentcs.com/release/"
 let apiKey = "fahi343asfas4g"
+
