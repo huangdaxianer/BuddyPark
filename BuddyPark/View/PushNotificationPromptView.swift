2a4233ab618b7c8920e93c34ca5fd027e574e3b0
huangdaxianer <huang0623@gmail.com>
2024-02-20 00:27:39 +0800

    增加了一个处理推送通知的弹窗

---
+++ BuddyPark/View/PushNotificationPromptView.swift
@@ -0,0 +1,18 @@
+//
+//  PushNotificationPromptView.swift
+//  BuddyPark
+//
+//  Created by 黄鹏昊 on 2024/2/19.
+//
+
+import SwiftUI
+
+struct PushNotificationPromptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PushNotificationPromptView()
+}
